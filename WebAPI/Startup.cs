add2d630b49232b65f243eb985655cdbaf5c8cd8
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-12-12 02:30:29 +0300

    12.12.2021 Eğitim Sonu Kaydı

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -37,6 +37,12 @@ namespace WebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
             });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("http://localhost:4200"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +54,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
